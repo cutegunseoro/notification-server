0813224476ae772324c71e0401d856d476db7b1a
Gwon Hong <gwonhong.dev@gmail.com>
2024-11-26 03:08:35 +0900

    init

---
+++ src/test/java/com/ssafy/travlog/notification/NotificationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.travlog.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
